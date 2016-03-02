56724bdd98c3f6f6634a3650c920eebbec4a2a42
Zheondre <Angel.Calcano@philips.com>
2016-03-02 15:22:56 -0500

    moved model from app_server to app_api

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/model/db.js'); 
+require('./app_api/model/db.js'); 
 
 var routes = require('./app_server/routes/index');
 //var routesApi = require('./app_api/routes/index');
